3e4fca17decbef6ec144970057476d3dd212167d
Fabio-Matos1303 <fabiosmo123@gmail.com>
2023-08-21 16:50:24 -0300

    Tipos e variáveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Tipos primitivos
+         */
+        final int ANO_DE_NASCIMENTO = 2001;
+        int idade = 22;
+        double altura = 1.85;
+    }
+}
